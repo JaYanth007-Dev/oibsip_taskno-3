7d29905b221587d04dbc3d8a687c7155930d18eb
Jayanth Kumar <jayanth826833@gmail.com>
2023-07-15 21:34:34 +0530

    code modified

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -2,26 +2,33 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { v4 as uuidv4 } from "uuid";
+
 const AddTodo = ({ dispatch }) => {
   const [title, setTitle] = useState("");
 
   const handleAddTodo = () => {
+    if (!title) {
+      alert("Please enter the title");
+      return; 
+    }
+
     const newTodo = {
       id: uuidv4(),
       title,
       completed: false,
     };
+
     dispatch({ type: "ADD_TODO", payload: newTodo });
     setTitle("");
   };
 
   return (
-    <div className="flex flex-wrap  justify-center space-y-4 md:flex flex-row md:space-y-0">
+    <div className="flex flex-wrap justify-center space-y-4 md:flex flex-row md:space-y-0">
       <input
         type="text"
         placeholder="Enter a todo..."
         value={title}
-        className=" w-[300px] px-[15px] py-[10px]  text-[20px] bg-[#000] border-2 border-[#c89666]  outline-none rounded-full text-[#ccc] shadow1 whitespace-normal mx-auto md:mr-[25px]"
+        className="w-[300px] px-[15px] py-[10px] text-[20px] bg-[#000] border-2 border-[#c89666] outline-none rounded-full text-[#ccc] shadow1 whitespace-normal mx-auto md:mr-[25px]"
         onChange={(e) => setTitle(e.target.value)}
       />
       <button
